66fa30afb78035f87c72d8694b5a2db38c938d66
AffanKhurram <affankhurram1@gmail.com>
2021-11-29 21:28:53 -0800

    used api

--- setup.py
+++ setup.py
@@ -1,29 +1,23 @@
 from mininet.net import Mininet
-from mininet.topo import Topo
+from mininet.topo import SingleSwitchTopo
 from mininet.cli import CLI
 import time
 
-# Setup a basic network with two hosts s and r
-# Both hosts are connected to a switch s0
-net = Mininet()
-c0 = net.addController()
-s = net.addHost('s')
-r = net.addHost('r')
-
-s0 = net.addSwitch('s0')
-
-net.addLink(s, s0)
-net.addLink(r, s0)
-
-
-s.setIP('192.168.1.1', 24)
-r.setIP('192.168.1.2', 24)
+# Sets up network with two hosts connected to a switch 
+topo = SingleSwitchTopo()
+net = Mininet(topo=topo)
 
 # Start the network
 net.start()
 net.pingAll()
 
-# Run s.py on the host s
+nodes = net.items()
+s = net.get('h1')
+r = net.get('h2')
+
+s.setIP('192.168.1.1', 24)
+r.setIP('192.168.1.2', 24)
+
 s.cmd('python3 s.py > out.txt &')
 time.sleep(5)
 result = r.cmd('python3 client.py')
