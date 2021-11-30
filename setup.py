a22d38049e8d7c878b299ba2ae834e4f32d1a0c7
AffanKhurram <affankhurram1@gmail.com>
2021-11-29 21:06:41 -0800

    server client code

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from mininet.net import Mininet
 from mininet.topo import Topo
 from mininet.cli import CLI
+import time
 
 # Setup a basic network with two hosts s and r
 # Both hosts are connected to a switch s0
@@ -16,9 +17,14 @@ net.addLink(r, s0)
 
 # Start the network
 net.start()
+s.setIP('192.168.1.1', 24)
+r.setIP('192.168.1.2', 24)
+net.pingAll()
 
 # Run s.py on the host s
-result = s.cmd('python3 s.py')
+s.cmd('python3 s.py > out.txt &')
+time.sleep(5)
+result = r.cmd('python3 client.py')
 print(result)
 
 net.stop()
